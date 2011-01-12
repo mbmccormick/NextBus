17c88a9b7fcbf6bab4e7d08a49d513c10295ef9f
Matt McCormick <mbmccormick@gmail.com>
2011-01-11 20:44:07 -0500

    Finished implementation of new user interface and added ability to change stops.

--- cookies.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    echo "Stops: " . $_COOKIE["stops"] . "<br />";
-    
-?>
--- index.php
+++ index.php
@@ -2,20 +2,38 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>NextBus WEB</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="/css/style.css" />
     <META HTTP-EQUIV="Refresh" CONTENT="60">
 </head>
 <body>
+    <?php
+    
+        if (isset($_GET[stop]) == true)
+            $data = simplexml_load_file("http://myride.gocitybus.com/widget/Default1.aspx?pt=30&code=" . $_GET[stop]);
+        else
+            $data = simplexml_load_file("http://myride.gocitybus.com/widget/Default1.aspx?pt=30&code=BUS271");
+    
+    ?>
     <div class="page">
         <div class="board">
-            <h1 onclick="location.href = location.href;">
+            <div class="header">
                 <table cellpadding="0" cellspacing="0" style="width: 100%;">
                     <tr valign="middle">
-                        <td><img src="logo.png" /></td>
-                        <td align="left" style="width: 100%; padding-top: 2px; padding-left: 10px;">NextBus Departures</td>
+                        <td>
+                            <img src="/img/logo.png" />
+                        </td>
+                        <td align="left" style="width: 40%; padding-top: 2px; padding-left: 10px;">
+                            <a href="/">NextBus Departures</a><br />
+                            <p>Powered by CityBus MyRide</p>
+                        </td>
+                        <td align="right" style="width: 60%; padding-top: 4px;">
+                            <div class="stop">
+                                <a onClick="changeStop();" style="cursor: pointer;" title="Click here to change your bus stop."><?php echo str_replace("(Shelter)", "", $data[name]); ?></a>
+                            </div>
+                        </td>
                     </tr>
                 </table>
-            </h1>
+            </div>
             <div class="legend">
                 <span class="route">Route</span>
                 <span class="name">Name</span>
@@ -24,8 +42,6 @@
             <ul>
                 <?php
                 
-                    $data = simplexml_load_file("http://myride.gocitybus.com/widget/Default1.aspx?pt=30&code=" . $_GET[stop]);
-                        
                     if ($data->Bus != null)
                     {
                         foreach ($data->Bus as $t)
@@ -50,7 +66,7 @@
                             else if ($diffmins < 20)
                                 $departure = floor($diffmins) . " minutes";
                             else
-                                $departure = "En Route";
+                                $departure = date("g:i A", $arritime);;
                             
                             echo "<span class='route'><span class='strikethrough'></span><strong>" . $route . "</strong></span>\n";
                             echo "<span class='name'><span class='strikethrough'></span><strong>" . $name . "</strong></span>\n";
@@ -74,5 +90,15 @@
             Copyright &copy; 2011 <a target="_blank" href="http://www.mccormicktechnologies.com/" style="text-decoration: none;">McCormick Technologies LLC</a>. All rights reserved.
         </div>
     </div>
+    <script type="text/javascript">
+    
+        function changeStop()
+        {
+            var stop = prompt("Please enter the QuickCode (BUS###) for your bus stop.");
+            if (stop != null)
+                location.href = "http://nextbuslaf.com/?stop=" + stop;
+        }
+    
+    </script>
 </body>
 </html>
